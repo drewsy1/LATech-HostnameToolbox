916047747fe41317dcfb0db0aa01c6b79129c29b
Drew Westgate <drew.westgate@outlook.com>
2019-02-12 01:14:55 -0600

    Got generation of conversion page elements working

--- LATech-HostnameToolbox/MainWindow.xaml.cs
+++ LATech-HostnameToolbox/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using Schemas;
 using System.IO;
 using System.Collections;
 using System.Reflection;
+using System.Windows.Markup;
 
 namespace LATech_HostnameToolbox
 {
@@ -105,47 +106,34 @@ namespace LATech_HostnameToolbox
             labelCurrentFilePath.Content = XMLFileName;
             labelCurrentFilePath.ToolTip = XMLFilePath;
 
-            while (tabControlPDU.Items.Count > 0) { tabControlPDU.Items.RemoveAt(0); }
+            tabControlPDU.Items.Clear();
+            gridHostnameConverterControls.Children.Clear();
+            if(gridHostnameConverterControls.ColumnDefinitions.Count > 1)
+                gridHostnameConverterControls.ColumnDefinitions.RemoveRange(1, gridHostnameConverterControls.ColumnDefinitions.Count - 1);
 
-            foreach (PredefinedUnitsTypePredefinedUnit PDU in _XMLProcessor.PredefinedUnits)
+
+            DataGridCollection dataGrids = new DataGridCollection(_XMLProcessor.PredefinedUnits);
+
+            List<TabItem> dataGridTabItems = dataGrids.ToTabItems();
+            dataGridTabItems.ForEach(x => tabControlPDU.Items.Add(x));
+
+            List<Control[]> dataGridControls = dataGrids.ToGridObjects();
+            for (int i = 0; i < dataGridControls.Count; i++)
             {
-                List<PDUItem> PDUItems = new List<PDUItem>();
-                foreach (PredefinedUnitsTypePredefinedUnitItem item in PDU.Item)
+                Control[] currentGridGroup = dataGridControls[i];
+                gridHostnameConverterControls.Children.Add(currentGridGroup[0]);
+                gridHostnameConverterControls.Children.Add(currentGridGroup[1]);
+                gridHostnameConverterControls.Children.Add(currentGridGroup[2]);
+
+                if (i < dataGridControls.Count - 1)
                 {
-                    PDUItem NewPDUItem = new PDUItem(item);
-                    PDUItems.Add(NewPDUItem);
+                    List<ColumnDefinition> newColumns = new List<ColumnDefinition> {
+                                new ColumnDefinition { Width = new GridLength(5) },
+                                new ColumnDefinition { Width = new GridLength(1,GridUnitType.Star) }
+                            };
+                    newColumns.ForEach(col => gridHostnameConverterControls.ColumnDefinitions.Add(col));
                 }
-
-                var columns = PDUItems.First()
-                    .Properties
-                    .Select((x, i) => new { x.Name, Index = i })
-                    .ToArray();
-
-                DataGrid tempDataGrid = new DataGrid
-                {
-                    HorizontalAlignment = HorizontalAlignment.Stretch,
-                    VerticalAlignment = VerticalAlignment.Stretch,
-                    AutoGenerateColumns = false,
-                    ItemsSource = PDUItems,
-                    IsReadOnly = true
-                };
-
-                foreach (var column in columns)
-                {
-                    var binding = new Binding(string.Format("Properties[{0}].Value", column.Index));
-
-                    tempDataGrid.Columns.Add(new DataGridTextColumn() { Header = column.Name, Binding = binding });
-                }
-                tempDataGrid.Columns.Last().Width = new DataGridLength(1, DataGridLengthUnitType.Star);
-
-                TabItem tempTabItem = new TabItem
-                {
-                    Header = PDU.Name,
-                    Content = tempDataGrid
-                };
-
-                tabControlPDU.Items.Add(tempTabItem);
-            }
+            };
         }
 
         public string GetResourceTextFile(string filename)
@@ -163,6 +151,101 @@ namespace LATech_HostnameToolbox
             return result;
         }
 
-        
+        private class DataGridCollection
+        {
+            public List<DataGrid> _dataGrids1 = new List<DataGrid>();
+            public List<DataGrid> _dataGrids2 = new List<DataGrid>();
+
+            public DataGridCollection(List<PredefinedUnitsTypePredefinedUnit> PDUs)
+            {
+                _dataGrids1.Clear();
+                _dataGrids2.Clear();
+                foreach (PredefinedUnitsTypePredefinedUnit PDU in PDUs)
+                {
+                    List<PDUItem> PDUItems = new List<PDUItem>();
+                    foreach (PredefinedUnitsTypePredefinedUnitItem item in PDU.Item)
+                    {
+                        PDUItem NewPDUItem = new PDUItem(item);
+                        PDUItems.Add(NewPDUItem);
+                    }
+
+                    var columns = PDUItems.First()
+                        .Properties
+                        .Select((x, i) => new { x.Name, Index = i })
+                        .ToArray();
+
+                    DataGrid tempDataGrid1 = new DataGrid
+                    {
+                        Tag = PDU.Name,
+                        HorizontalAlignment = HorizontalAlignment.Stretch,
+                        VerticalAlignment = VerticalAlignment.Stretch,
+                        AutoGenerateColumns = false,
+                        ItemsSource = PDUItems,
+                        IsReadOnly = true
+                    };
+                    DataGrid tempDataGrid2 = new DataGrid
+                    {
+                        Name = "dataGrid" + PDU.Name.Replace(" ", ""),
+                        Tag = PDU.Name,
+                        HorizontalAlignment = HorizontalAlignment.Stretch,
+                        VerticalAlignment = VerticalAlignment.Stretch,
+                        AutoGenerateColumns = false,
+                        ItemsSource = PDUItems,
+                        IsReadOnly = true
+                    };
+
+                    foreach (var column in columns)
+                    {
+                        var binding = new Binding(string.Format("Properties[{0}].Value", column.Index));
+
+                        tempDataGrid1.Columns.Add(new DataGridTextColumn() { Header = column.Name, Binding = binding });
+                        tempDataGrid2.Columns.Add(new DataGridTextColumn() { Header = column.Name, Binding = binding });
+                    }
+                    tempDataGrid1.Columns.Last().Width = new DataGridLength(1, DataGridLengthUnitType.Star);
+                    tempDataGrid2.Columns.Last().Width = new DataGridLength(1, DataGridLengthUnitType.Star);
+
+                    this._dataGrids1.Add(tempDataGrid1);
+                    this._dataGrids2.Add(tempDataGrid2);
+                }
+            }
+
+            public List<TabItem> ToTabItems()
+            {
+                List<TabItem> TabItems = _dataGrids1.ConvertAll((DataGrid x) => new TabItem{Header = x.Tag,Content = x});
+                return TabItems;
+            }
+
+            public List<Control[]> ToGridObjects()
+            {
+                int currentCol = 0;
+                return _dataGrids2.ConvertAll(currentGrid =>
+                    {
+                        Label newLabel = new Label() {
+                            Name = "label" + currentGrid.Tag.ToString().Replace(" ", ""),
+                            Content = currentGrid.Tag.ToString()
+                        };
+                        newLabel.SetValue(Grid.RowProperty, 0);
+                        newLabel.SetValue(Grid.ColumnProperty, currentCol);
+
+                        TextBox newTextBox = new TextBox() {
+                            Name = "textBox" + currentGrid.Tag.ToString().Replace(" ", ""),
+                            Height = 23,
+                            TextWrapping = TextWrapping.Wrap,
+                            Text = "",
+                            VerticalAlignment = VerticalAlignment.Top
+                        };
+                        newTextBox.SetValue(Grid.RowProperty, 1);
+                        newTextBox.SetValue(Grid.ColumnProperty, currentCol);
+
+                        currentGrid.SetValue(Grid.RowProperty, 3);
+                        currentGrid.SetValue(Grid.ColumnProperty, currentCol);
+
+                        currentCol+=2;
+
+                        return new Control[] { newLabel, newTextBox, currentGrid };
+                    }
+                );
+            }
+        }
     }
 }
